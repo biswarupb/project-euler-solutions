550164b5cd36564181843c50e8662c03700806b8
Biswarup Bhattacharya <biswarup.biswajoy@gmail.com>
2014-05-08 13:53:58 +0530

    Solution 9

---
+++ Solutions/pe9.cpp
@@ -0,0 +1,80 @@
+/*
+
+Name: Biswarup Bhattacharya
+PE Handle: biswarupb
+Date: 08 May 2014
+Problem: 9
+
+*/
+
+#include <cstdio>
+#include <algorithm>
+#include <utility>
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#define MOD (10e9)+7
+#define gc getchar()
+#define pc putchar
+using namespace std;
+typedef long long ll;
+
+//Fast read
+inline void read(ll& n)
+{
+	n = 0;
+	char ch = gc; int sign = 1;
+	while(ch < '0' || ch > '9'){if(ch == '-') sign = -1; ch = gc; }
+	while(ch >= '0' && ch<= '9')
+	{
+		n = n*10 + ch - '0';
+		ch = gc;
+	}
+	n *= sign;
+}
+
+//Fast print
+inline void print(ll n)
+{
+	char ch[15];
+	int ind=0;
+	if(n<0)
+    {
+        pc('-');
+        n *= -1;
+    }
+	if(n == 0)
+	{
+		pc('0');
+	}
+	while(n > 0)
+	{
+		ch[ind] = n%10 + '0';
+		n /= 10;
+		ind++;
+	}
+	while(ind--) pc(ch[ind]);
+	pc('\n');
+}
+
+int main()
+{
+    ll a,b,c;
+    for(a=3; ;a++)
+    {
+        for(b=3;;b++)
+        {
+            c=1000-a-b;
+            if(c<0)
+                break;
+            if(((a*a)+(b*b))==(c*c))
+            {
+                print(a*b*c);
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
